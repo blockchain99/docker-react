d35eab4a0645c06b16225898b3ee90232d405846
YOONSU PARK <shinebytheriver@yahoo.com>
2019-02-04 17:13:50 -0800

    branch feature2!

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           changed for master branch: Hello !!!
+           I changed on the Feature branch !!
           </p>
           
         </header>
