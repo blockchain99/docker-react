b04dc2b6a8969c66c2f7c59294b3a4d911a4e1dd
YOONSU PARK <shinebytheriver@yahoo.com>
2019-02-07 17:33:28 -0800

    App.js changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-           changed for master branch: Hello !!!
+           changed for master branch updated : Hi !!!
           </p>
           
         </header>
